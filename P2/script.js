7f0ed09eba92ba773833085fc49316fbb402ec6a
Prakash Raj <prakashrajk274@gmail.com>
2025-06-05 19:13:06 +0530

    update

--- P2/script.js
+++ P2/script.js
@@ -17,7 +17,9 @@ const showRegister = () => {
     <p><input type="text" id="txtName"></p>
      <p><input type="text" id="txtEmail"></p>
     <p><input type="password" id="txtPass"></p>
-    <button>Register</button>
+    <button onclick="showLogin()">Register</button>
+    
+    
     `
     root.innerHTML = str
 }
